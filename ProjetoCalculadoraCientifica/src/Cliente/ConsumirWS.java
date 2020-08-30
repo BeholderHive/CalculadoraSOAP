244aa82302071ba8e8b19026e8d8d1aa1f51ef76
BeholderHive <54648118+BeholderHive@users.noreply.github.com>
2020-08-29 22:33:40 -0300

    Add files via upload

---
+++ ProjetoCalculadoraCientifica/src/Cliente/ConsumirWS.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cliente;
+
+import Servidor.BaseInterfaceWS;
+import java.net.URL;
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+/**
+ *
+ * @author maxwe
+ */
+public class ConsumirWS {
+    public static void main (String[] args) throws Exception{
+        URL url=new URL("http://localhost:9999/Servidor/ServidorMensagens?wsdl");
+        QName qname=new QName("http://Servidor/", "ImplementacaoDaBaseInterfaceWSService");
+        
+        Service service=Service.create(url, qname);
+        
+        BaseInterfaceWS receptorCliente=service.getPort(BaseInterfaceWS.class);
+        
+        System.out.println(receptorCliente.getServico("Potencia", 2, 3));
+        System.out.println(receptorCliente.getCatalogo());
+        System.out.println(receptorCliente.getServico("Dividir", 10, 5));
+        System.out.println(receptorCliente.getCatalogo());
+    }
+}
